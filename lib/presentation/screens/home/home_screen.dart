7bcdd72e8fdb3c22fc5316575b710c1faa234787
angelina_pg <angelinapinto1278@gmail.com>
2024-06-17 11:38:07 -0600

    Implementacion de listTile

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -21,18 +21,41 @@ class _homeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return ListView.builder(
+      physics: BouncingScrollPhysics(),
       itemCount: appMenuItems.length,
       itemBuilder: (context, index){
         //recorriendo lista de appMenuItems 
         final menuItem = appMenuItems[index];
         //obteniendo el valor de objeto 
-        return ListTile(
-          title: Text(menuItem.title),
-          subtitle: Text(menuItem.subtitle),
-        );
+        return _CustomListTile(menuItem: menuItem);
       },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    super.key,
+    required this.menuItem,
+  });
+
+  final MenuItems menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary,),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subtitle),
+      onTap: () {
+        
+      },
+
     );
   }
 }
